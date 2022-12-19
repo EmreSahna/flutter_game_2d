e179a06abd7a842990e49367c31d2337f0fbbf7a
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-12-19 20:56:08 +0300

    game state fix.

--- lib/game.dart
+++ lib/game.dart
@@ -1,56 +1,61 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'initalize.dart';
 
-class Game extends StatefulWidget{
+class Game extends StatefulWidget {
   const Game({super.key});
   @override
   State<StatefulWidget> createState() => _GameScreenState();
 }
 
-class _GameScreenState extends State<Game>{
+class _GameScreenState extends State<Game> {
+  var currentStage = 1;
 
   @override
   Widget build(BuildContext context) {
     final double _width = MediaQuery.of(context).size.width;
     final double _height = MediaQuery.of(context).size.height;
-    var defaultSettings = ModalRoute.of(context)!.settings.arguments as DefaultSettings;
-    var currentStage = 1;
 
-    bool checkStage(){
+    var defaultSettings =
+        ModalRoute.of(context)!.settings.arguments as DefaultSettings;
+
+    bool checkStage() {
       return defaultSettings.stages <= currentStage ? true : false;
     }
 
-    bool checkCharacter(){
+    bool checkCharacter() {
       return defaultSettings.character.health! > 0 ? true : false;
     }
 
-    bool checkMob(){
+    bool checkMob() {
       return defaultSettings.enemy.health! > 0 ? true : false;
     }
 
-    void gameLogic(){
-      if(checkCharacter()){
-        if(checkMob()){
+    void gameLogic() {
+      if (checkCharacter()) {
+        if (checkMob()) {
           print('mob alive');
-        }else{
+        } else {
           print('you kill mob');
           defaultSettings.enemy.health = 10;
-          if(checkStage()){
+          if (checkStage()) {
             print('finished game');
-          }else{
+          } else {
             print('still playing');
           }
         }
-      }else{
+      } else {
         print('reset game');
       }
     }
 
-    void doAttack(){
-      defaultSettings.enemy.health = defaultSettings.enemy.health! - defaultSettings.character.damage!;
+    void doAttack() {
+      defaultSettings.enemy.health =
+          defaultSettings.enemy.health! - defaultSettings.character.damage!;
       gameLogic();
       setState(() {
-        currentStage = currentStage + 1;      
+        currentStage = currentStage + 1;
         print(currentStage);
       });
     }
@@ -70,39 +75,42 @@ class _GameScreenState extends State<Game>{
               ),
             ),
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(defaultSettings.character.health.toString(), 
-                  style: const TextStyle(color: Colors.red, 
-                    fontSize: 55,
-                    fontWeight: FontWeight.bold
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    defaultSettings.character.health.toString(),
+                    style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                Column(
-                  children: [
-                    Text(currentStage.toString(),
-                      style: const TextStyle(color: Colors.amber,
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold
+                  Column(
+                    children: [
+                      Text(
+                        currentStage.toString(),
+                        style: const TextStyle(
+                            color: Colors.amber,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Text(defaultSettings.username,
-                      style: const TextStyle(color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold
-                      ),
-                    )
-                  ],
-                ),
-                Text(defaultSettings.enemy.health.toString(),
-                  style: const TextStyle(color: Colors.red,
-                    fontSize: 55,
-                    fontWeight: FontWeight.bold
+                      Text(
+                        defaultSettings.username,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
                   ),
-                ),
-              ]
-            ),
+                  Text(
+                    defaultSettings.enemy.health.toString(),
+                    style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ]),
           ),
           Container(
             height: 240.0,
@@ -114,19 +122,22 @@ class _GameScreenState extends State<Game>{
               ),
             ),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Padding(
-                  padding:  EdgeInsets.only(bottom: _height * 0.025),
-                  child: Image.asset(defaultSettings.character.imagePath(),scale: 0.3),
-                ),
-                Image.asset(defaultSettings.enemy.imagePath(), scale: 1.3,)
-              ]
-            ),
-          ),  
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: _height * 0.025),
+                    child: Image.asset(defaultSettings.character.imagePath(),
+                        scale: 0.3),
+                  ),
+                  Image.asset(
+                    defaultSettings.enemy.imagePath(),
+                    scale: 1.3,
+                  )
+                ]),
+          ),
           Container(
-            padding: const EdgeInsets.only(bottom: 10,left: 10),
+            padding: const EdgeInsets.only(bottom: 10, left: 10),
             height: _height * 0.3,
             width: _width,
             decoration: const BoxDecoration(
@@ -135,42 +146,40 @@ class _GameScreenState extends State<Game>{
                 fit: BoxFit.fill,
               ),
             ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    doAttack();
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.only(right: 10),
-                    height: _height*0.15,
-                    width: _width*0.3,
-                    decoration: BoxDecoration(
+            child: Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              GestureDetector(
+                onTap: () {
+                  doAttack();
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  height: _height * 0.15,
+                  width: _width * 0.3,
+                  decoration: BoxDecoration(
                       color: Colors.orange,
-                      borderRadius: BorderRadius.circular(60)
-                    ),
-                    child: const Center(child: Text("Attack",style: TextStyle(color: Colors.black, fontSize: 30))),
-                  ),
+                      borderRadius: BorderRadius.circular(60)),
+                  child: const Center(
+                      child: Text("Attack",
+                          style: TextStyle(color: Colors.black, fontSize: 30))),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                    });
-                  },
-                  child: Container(
-                    height: _height*0.15,
-                    width: _width*0.3,
-                    decoration: BoxDecoration(
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {});
+                },
+                child: Container(
+                  height: _height * 0.15,
+                  width: _width * 0.3,
+                  decoration: BoxDecoration(
                       color: Colors.green,
-                      borderRadius: BorderRadius.circular(60)
-                    ),
-                    child: const Center(child: Text("Heal",style: TextStyle(color: Colors.black, fontSize: 30))),
-                  ),
+                      borderRadius: BorderRadius.circular(60)),
+                  child: const Center(
+                      child: Text("Heal",
+                          style: TextStyle(color: Colors.black, fontSize: 30))),
                 ),
-              ]
-            ),
-          )  
+              ),
+            ]),
+          )
         ],
       ),
     );
